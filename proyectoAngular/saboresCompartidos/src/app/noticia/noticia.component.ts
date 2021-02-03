1431f664c64cc338c294e0f9c768b0dff68afb92
David Santistevan <dsantistevana99@gmail.com>
2021-02-03 18:05:10 -0500

    Llamado al api de la pantalla de noticias (incompleto)

--- proyectoAngular/saboresCompartidos/src/app/noticia/noticia.component.ts
+++ proyectoAngular/saboresCompartidos/src/app/noticia/noticia.component.ts
@@ -1,25 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import * as introJs from 'intro.js';
 
+
 @Component({
   selector: 'app-noticia',
   templateUrl: './noticia.component.html',
   styleUrls: ['./noticia.component.css']
 })
 export class NoticiaComponent implements OnInit {
-  introJs = introJs()
+  introJs = introJs();
+  noticia: any;
   constructor() { }
 
+  
+
   ngOnInit(): void {
-    window.addEventListener('load', function() {
-      const queryString = window.location.search;
-      const urlParams = new URLSearchParams(queryString);
-      if (urlParams.get('tutorial') == 'true'){
-          introJs().setOption('doneLabel', 'Ir a recetas').start().oncomplete(function() {
-              window.location.href = '/recetas?tutorial=true';
-          });
-      }
-    });
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    fetch('https://saborescompartidos.herokuapp.com/api/v1/news/'+urlParams.get('id'))
+      .then( (resultado) => {
+        return resultado.json()
+      }) 
+      .then( (jsn) => {
+        this.noticia = jsn
+        console.log(this.noticia)
+      })
+      .catch( (error) => {
+        console.log("Error ",error)
+      })
+      .then( () => {
+        window.addEventListener('load', function() {
+          const queryString = window.location.search;
+          const urlParams = new URLSearchParams(queryString);
+          if (urlParams.get('tutorial') == 'true'){
+              introJs().setOption('doneLabel', 'Ir a recetas').start().oncomplete(function() {
+                  window.location.href = '/recetas?tutorial=true';
+              });
+          }
+        });
+    })
+    
   }
 
 }
