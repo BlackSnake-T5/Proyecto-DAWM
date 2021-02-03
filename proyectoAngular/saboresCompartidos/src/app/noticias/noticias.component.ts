acbc4139c85989ee7050ba20819c9677bdb52b59
David Santistevan <dsantistevana99@gmail.com>
2021-02-03 18:02:49 -0500

    Uso del api en la pantalla de noticias

--- proyectoAngular/saboresCompartidos/src/app/noticias/noticias.component.ts
+++ proyectoAngular/saboresCompartidos/src/app/noticias/noticias.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import * as introJs from 'intro.js';
 
 @Component({
@@ -8,18 +9,42 @@ import * as introJs from 'intro.js';
 })
 export class NoticiasComponent implements OnInit {
   introJs = introJs()
-  constructor() { }
+  noticias: any[] | undefined;
+  constructor(private _router: Router) { }
 
   ngOnInit(): void {
-    window.addEventListener('load', function() {
-      const queryString = window.location.search;
-      const urlParams = new URLSearchParams(queryString);
+    
+    
+    fetch('https://saborescompartidos.herokuapp.com/api/v1/news/')
+      .then( (resultado) => {
+        console.log(resultado)
+        return resultado.json()
+      }) 
+      .then( (jsn) => {
+        this.noticias = Array.of(jsn)[0]
+        console.log(this.noticias)
+      })
+      .catch( (error) => {
+        console.log("Error ",error)
+      })
+      .then( () => {
+  
+      window.addEventListener('load', function() {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
 
-      if (urlParams.get('tutorial') == 'true'){
-          introJs().setOption('doneLabel', 'Ver noticia').start().oncomplete(function() {
-              window.location.href = '/noticia?tutorial=true';
-          });
-      }
+        if (urlParams.get('tutorial') == 'true'){
+            introJs().setOption('doneLabel', 'Ver noticia').start().oncomplete(function() {
+                window.location.href = '/noticia?tutorial=true';
+            });
+        }
+      });
+    })
+  }
+
+  noticiaDetails(id : string){
+    this._router.navigate(['noticia'], {
+      queryParams: {'id': id}
     });
   }
 
